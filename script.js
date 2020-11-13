129576bf436e0b6335c0def7f8ef6baa6261bce6
Mikey <michael.luevanos@gmail.com>
2020-11-13 14:37:52 -0800

    cleaned up comments in JS doc and removed some unecessary css

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 var baseUrl = 'http://api.worldweatheronline.com/premium/v1/weather.ashx?q=';
-var apiKey = 'd22e5db08ccb4da289d190837201211';
-
-var testUrl = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=Los Angeles&format=json&num_of_days=1&date=today&includelocation=yes&key=" + apiKey;
-
 var tailBaseUrl = '&format=json&num_of_days=1&date=today&includelocation=yes&key=d22e5db08ccb4da289d190837201211';
-//test to make sure the API is accessible and working
-// $.ajax({
-//     url: testUrl,
-//     method: "GET"
-// })
-// .then(function(response) {
-//     console.log(testUrl);
-//     console.log(response);
-// })
-
-//new base Url to allow space for manual input
-//var baseUrlV2 = "http://api.worldweatheronline.com/premium/v1/weather.ashx?q=" + localInput + "&format=json&num_of_days=1&date=today&includelocation=yes&key=" + apiKey;
 
 $("#confirm").on("click", function() {
     $(".container3").show();
     let localInput = document.querySelector(".myLocation").value;
     console.log(localInput);
-    let queryTest = baseUrl + localInput + tailBaseUrl;
-    console.log(queryTest);
+
+    let queryUrl = baseUrl + localInput + tailBaseUrl;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+    .then(function(response) {
+        //to test the url and response from weather API
+        console.log(queryUrl);
+        console.log(response.data);
+
+        //setting current weather conditions to variables which will be used later
+        let responseData = response.data.current_condition[0];
+        let tempNowFar = responseData.temp_F;
+        let weatherCode = responseData.weatherCode;
+        console.log(tempNowFar);
+        console.log(weatherCode);
+
+        let currentDesc = responseData.weatherDesc[0].value;
+        let tempIcon = responseData.weatherIconUrl[0].value;
+        console.log(currentDesc);
+        console.log(tempIcon);
+
+        //Displaying current weather conditions on the page
+        $(".openWeather").prepend("<hr>" + "Current Temperature : " + tempNowFar + "F" + "<br>" + "Current Condition : " + currentDesc );
+        $(".icon-holder").attr("src", tempIcon);
+    })
+
 })
 
 
