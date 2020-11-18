8dca74df4984d7a2b7f26a3fadf5eb08abfb4213
Mikey <michael.luevanos@gmail.com>
2020-11-18 07:14:38 -0800

    all weather conditionals w/ recipes added

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $("#confirm").on("click", function() {
         console.log(queryUrl);
         console.log(response.data);
 
-        //setting current weather conditions to variables which will be used later
+        //setting current weather conditions to variables which will be displayed
         let responseData = response.data.current_condition[0];
         let tempNowFar = responseData.temp_F;
         let weatherCode = responseData.weatherCode;
@@ -54,17 +54,124 @@ $("#confirm").on("click", function() {
                     console.log(recipeImg);
                     let recipeText = recipeName.link(recipeUrl);
                     let recipeIcon = $("<img>").attr("src", recipeImg);
-                    picDiv.prepend("<br><hr>" + recipeText);
+                    picDiv.prepend("<br><hr>" + recipeText + "<br>");
                     picDiv.append(recipeIcon);
                     $(".food").append(picDiv);
                 }
             })
         }
-        else {
-            console.log("weather code 113 not appearing");
+        //setting the Cold weather conditional to surface appropriate recipes
+        else if (weatherCode === "323"|| weatherCode === "326" || weatherCode === "329" || weatherCode === "332" || weatherCode === "335" || weatherCode === "338") {
+            console.log("weather code is snow");
+            $("#windyVideo").show();
+            $("#homeVideo").hide();
+            let urlCold = "https://api.edamam.com/search?q=soup&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+            $.ajax({
+                url: urlCold,
+                method: "GET"
+            })
+            .then(function(response) {
+                console.log(response);
+                let recipeList = response.hits;
+                console.log(recipeList[0].recipe.label);
+                console.log(recipeList[0].recipe.shareAs);
+                for (i = 0; i < recipeList.length; i++) {
+                    let picDiv = $("<div>");
+                    let recipeName = recipeList[i].recipe.label;
+                    let recipeUrl = recipeList[i].recipe.shareAs;
+                    let recipeImg = recipeList[i].recipe.image;
+                    let recipeText = recipeName.link(recipeUrl);
+                    let recipeIcon = $("<img>").attr("src", recipeImg);
+                    picDiv.prepend("<br><hr>" + recipeText + "<br>");
+                    picDiv.append(recipeIcon);
+                    $(".food").append(picDiv);
+                }
+            })
         }
-    })
+        //setting the Rain weather conditional to surface appropriate recipes
+        else if (weatherCode ==="308" || weatherCode === "305" || weatherCode === "302" || weatherCode === "299" || weatherCode === "296" || weatherCode === "293") {
+            $("#rainVideo").show();
+            $("#homeVideo").hide();
+            let urlRain = "https://api.edamam.com/search?q=crockpot&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+            $.ajax({
+                url: urlRain,
+                method: "GET"
+            })
+            .then(function(response) {
+                console.log("rain response: " + response) 
+                let recipeList = response.hits;
+                console.log(recipeList[0].recipe.label);
+                console.log(recipeList[0].recipe.shareAs);
+                for (i = 0; i < recipeList.length; i++) {
+                    let picDiv = $("<div>");
+                    let recipeName = recipeList[i].recipe.label;
+                    let recipeUrl = recipeList[i].recipe.shareAs;
+                    let recipeImg = recipeList[i].recipe.image;
+                    let recipeText = recipeName.link(recipeUrl);
+                    let recipeIcon = $("<img>").attr("src", recipeImg);
+                    picDiv.prepend("<br><hr>" + recipeText + "<br>");
+                    picDiv.append(recipeIcon);
+                    $(".food").append(picDiv);
+                }
+            })
+        }
+        //setting the Snow weather conditional to surface appropriate recipes
+        else if(weatherCode === "122" || weatherCode === "230") {
+            $("#snowVideo").show();
+            $("#homeVideo").hide();
+            let urlSnow = "https://api.edamam.com/search?q=roast&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+            $.ajax({
+            url: urlSnow,
+            method: "GET"
+            })
+            .then(function(response) {
+                console.log(response);
+                let recipeList = response.hits;
+                console.log(recipeList[0].recipe.label);
+                console.log(recipeList[0].recipe.shareAs);
+                for (i = 0; i < recipeList.length; i++) {
+                    let picDiv = $("<div>");
+                    let recipeName = recipeList[i].recipe.label;
+                    let recipeUrl = recipeList[i].recipe.shareAs;
+                    let recipeImg = recipeList[i].recipe.image;
+                    let recipeText = recipeName.link(recipeUrl);
+                    let recipeIcon = $("<img>").attr("src", recipeImg);
+                    picDiv.prepend("<br><hr>" + recipeText + "<br>");
+                    picDiv.append(recipeIcon);
+                    $(".food").append(picDiv);
+                    }
+                })
+            }
+        //setting the conditional for other weather conditions under a cloudy gen response
+        else {
+            $("#cloudyVideo").show();
+            $("#homeVideo").hide();
+            let urlDessert = "https://api.edamam.com/search?q=dessert&app_id=c814663e&app_key=0c4b0756ea6a4474bc365916d73a84b7";
+                $.ajax({
+                url: urlDessert,
+                method: "GET"
+                })
+            .then(function(response) {
+                console.log(response);
+                let recipeList = response.hits;
+                console.log(recipeList[0].recipe.label);
+                console.log(recipeList[0].recipe.shareAs); //OR recipe.shareAs
+                for (i = 0; i < recipeList.length; i++){
+                    let picDiv = $("<div>");
+                    let recipeName = recipeList[i].recipe.label;
+                    let recipeUrl = recipeList[i].recipe.shareAs;
+                    let recipeImg = recipeList[i].recipe.image;
+                    let recipeText = recipeName.link(recipeUrl);
+                    let recipeIcon = $("<img>").attr("src", recipeImg);
+                    picDiv.prepend("<br><hr>" + recipeText + "<br>");
+                    picDiv.append(recipeIcon);
+                    $(".food").append(picDiv);
+            }
+        })
+    }
 
+
+    })
 })
 
 
