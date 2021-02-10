daec08403ebdd5fd75e9509039104acee4fa8710
Mikey <michael.luevanos@gmail.com>
2021-02-10 12:18:18 -0800

    updated another weather condition

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ const searchResponse = (response) => {
         }
 
         //setting the Cold weather conditional to surface appropriate recipes
-        else if (weatherCode === "323"|| weatherCode === "326" || weatherCode === "329" || weatherCode === "332" || weatherCode === "335" || weatherCode === "389") {
+        else if (weatherCode === "323"|| weatherCode === "326" || weatherCode === "329" || weatherCode === "332" || weatherCode === "335" || weatherCode === "389" || weatherCode === "200") {
             $("#windyVideo").show();
             $("#rainVideo").hide();
             $("#homeVideo").hide();
